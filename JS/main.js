77d70719c63e0dc54ee881f60c391fba4296b4a8
Carina Adrianzen <70672577+carinadyann@users.noreply.github.com>
2023-04-14 12:41:34 -0700

    moved html files back to main

---
+++ JS/main.js
@@ -0,0 +1,22 @@
+﻿//side nav
+function openNav() {
+    document.getElementById("mySidenav").style.width = "180px";
+    document.getElementById("main").style.marginLeft = "180px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
+//contact
+function copyFunction(emailId) {
+    // Get the text field
+    var copyText = document.getElementById(emailId).innerHTML;
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText);
+
+    // Alert the copied text
+    alert('"' + copyText + '" was copied to your clipboard');
+}
